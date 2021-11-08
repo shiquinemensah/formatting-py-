cbf0f960e179baaeec0b207d9d117312e919f6a5
shiquine <godchildshiquine@gmail.com>
2021-11-08 05:21:11 -0800

    format

---
+++ format.py
@@ -0,0 +1,29 @@
+#txt = "For only {price:.2f} dollars!"
+#print(txt.format(price = 49))
+
+#test = 'hello world this is a test for price {price:.2f}'
+#print (test.format(price = 49))
+
+#john = 'hello world {james:.2f}'
+#print(john.format(james = 73))
+
+#johndoe = 'hello world my name is {Sname} and I am {age}'
+#print(johndoe.format(Sname = 'james', age = 63))
+
+#test2 = 'My name is {0}, i am {1}'.format('john', 36)
+#test3 = 'My name is {}, I am {}'.format('john',36)
+
+#print(test2)
+
+# Format allows you to replace an element as well as change the variable within a string.
+# Once you have selected the variable that you wish to change you have to activate it by using the 'print ()' -
+# To activate the plugin.
+
+example = 'hello world my name is {sname} and I work for {james} company'
+print (example.format(sname = 'james', james = 'JD sports'))
+
+# I have assigned different variables as well as different strings to test its functionality.
+#
+
+
+
